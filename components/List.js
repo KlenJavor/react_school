f6473383504c8e277a6ff6d282dfbf60903e5898
unknown <Patricie.Suppala@gmail.com>
2020-12-25 00:21:19 +0200

    fetch json

--- components/List.js
+++ components/List.js
@@ -2,6 +2,17 @@ import React from "react";
 import { FlatList } from "react-native";
 import ListItem from "./ListItem";
 
+const url =
+  "https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json";
+let mediaArray = [];
+const loadMedia = async () => {
+  const response = await fetch(url);
+  const json = await response.json();
+  console.log(json);
+};
+
+loadMedia();
+
 const List = () => {
   return (
     <FlatList
