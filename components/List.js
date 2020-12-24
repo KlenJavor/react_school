06e1065e9ff29427fdd91f0df2191f55cb8fff2f
unknown <Patricie.Suppala@gmail.com>
2020-12-25 00:29:48 +0200

    useEffect

--- components/List.js
+++ components/List.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FlatList } from "react-native";
 import ListItem from "./ListItem";
 
 const url =
   "https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json";
-let mediaArray = [];
-const loadMedia = async () => {
-  const response = await fetch(url);
-  const json = await response.json();
-  console.log(json);
-};
-
-loadMedia();
 
 const List = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+
+  useEffect(() => {
+    {
+      const loadMedia = async () => {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+        setMediaArray(json);
+        console.log(json);
+      };
+      loadMedia();
+    }
+  }, []);
   return (
     <FlatList
       data={mediaArray}
