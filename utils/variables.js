6ca514ab1fa1a7a8d0929d07837a3828d29f35fb
unknown <Patricie.Suppala@gmail.com>
2021-02-05 14:50:36 +0200

    B import React, {useContext, useEffect, useState} from 'react';

--- utils/variables.js
+++ utils/variables.js
@@ -1,5 +1,5 @@
 const uploadsUrl = 'https://media-new.mw.metropolia.fi/wbma/uploads/';
 const baseUrl = 'https://media-new.mw.metropolia.fi/wbma/';
-const mediaUrl = 'https://media-new.mw.metropolia.fi/wbma/media/';
+const appIdentifier = 'Helply';
 
-export {uploadsUrl, baseUrl, mediaUrl};
+export {uploadsUrl, baseUrl, appIdentifier};
